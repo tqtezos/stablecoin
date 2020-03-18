e110e0df5b62cd5e4c1bb3c433a96523c78ade4d
Anton Myasnikov <awkure@protonmail.ch>
2020-03-18 23:52:49 +0300

    [#1] Add CI and linter scripts

---
+++ .buildkite/check-trailing-whitespace.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+files=$(git ls-files -- . | xargs grep --files-with-matches --binary-files=without-match '[[:blank:]]$')
+if [[ ! -z $files ]];then
+    echo '  Files with trailing whitespace found:'
+    for f in "${files[@]}"; do
+        echo "  * $f"
+    done
+    exit 1
+fi
