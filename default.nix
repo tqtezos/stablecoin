e6ef4de3593964e6e7b48c74767a7d9fcf9d5004
rvem <roman.melnikov@serokell.io>
2020-06-05 11:43:25 +0300

    [#52] Add weeder check
    Problem: We normally want our Haskell code to pass weeder and hlint
    checks, but this repo doesn't check it on CI.
    
    Solution: Add weeder step to CI pipeline.

--- default.nix
+++ default.nix
@@ -4,16 +4,26 @@
 { sources ? import ./nix/sources.nix
 , haskell-nix ? import sources."haskell.nix" { }
 , pkgs ? import sources.nixpkgs haskell-nix.nixpkgsArgs
+, weeder-hacks ? import sources.haskell-nix-weeder { inherit pkgs; }
 , ligo ? (import "${sources.ligo}/nix" { }).ligo-bin
 }:
 let
+  local-packages = [{
+      name = "stablecoin";
+      subdirectory = ".";
+  }];
+  local-packages-names = map (p: p.name) local-packages;
   project = pkgs.haskell-nix.stackProject {
     src = ./haskell;
     modules = [
       {
-        packages.stablecoin.components.tests.stablecoin-test = {
-          postInstall = "mkdir -p $out/test/resources; cp -Lr ${tezos-contract} $out/test/resources/stablecoin.tz";
-        };
+        packages = pkgs.lib.genAttrs local-packages-names (packageName: {
+            package.ghcOptions = with pkgs.lib;
+              concatStringsSep " " ([
+                "-ddump-to-file" "-ddump-hi"
+              ]);
+            postInstall = weeder-hacks.collect-dump-hi-files;
+        });
       }
     ];
   };
@@ -25,11 +35,15 @@ let
     installPhase = "cp stablecoin.tz $out";
   };
   tezos-client = (import "${sources.tezos-packaging}/pkgs.nix" {}).ocamlPackages.tezos-client;
+  weeder-script = weeder-hacks.weeder-script {
+    hs-pkgs = project;
+    local-packages = local-packages;
+  };
 in
 {
   all = project.stablecoin.components.all;
   lib = project.stablecoin.components.library;
   test = project.stablecoin.components.tests.stablecoin-test;
   nettest = project.stablecoin.components.tests.stablecoin-nettest;
-  inherit tezos-contract tezos-client pkgs;
+  inherit tezos-contract tezos-client pkgs weeder-script;
 }
