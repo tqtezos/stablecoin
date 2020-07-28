49b4c61c4583226d9469b7f139b7a8120818fea7
Anton Myasnikov <awkure@protonmail.ch>
2020-07-28 12:47:45 +0300

    [#75] Update nettest scenarios for external transferlist interaction
    Problem: We want to test external transferlist with out contract
    in a real network.
    
    Solution: Update existing nettest scenarios to be able to use any
    transferlist contract for interaction.

--- haskell/nettest/Main.hs
+++ haskell/nettest/Main.hs
@@ -7,15 +7,21 @@ module Main
 import Options.Applicative (execParser)
 import qualified Unsafe (fromJust)
 
-import Lorentz (compileLorentzContract)
-import Michelson.Runtime (prepareContract)
+import qualified Indigo.Contracts.Transferlist.External as External
+import qualified Indigo.Contracts.Transferlist.Internal as Internal
+import Lorentz (ToT, compileLorentzContract, toVal)
+import Lorentz.Contracts.Test.Common
+import Michelson.Runtime (parseExpandContract, prepareContract)
+import Michelson.Typed (untypeValue)
 import Michelson.Typed.Convert (convertContract)
 import Morley.Nettest
+import Nettest (scNettestScenario)
+import Tezos.Address
+import Util.IO
 import Util.Named
 
-import Indigo.Contracts.Safelist (safelistContract)
-import Lorentz.Contracts.Test.Common
-import Nettest (scTransferScenario)
+externalTransferlistContractPath :: FilePath
+externalTransferlistContractPath = "test/resources/transferlist.tz"
 
 main :: IO ()
 main = do
@@ -23,18 +29,47 @@ main = do
   parsedConfig <- execParser $
     parserInfo
       (#usage .! mempty)
-      (#description .! "Stablecoin nettest scenario")
+      (#description .! "Stablecoin nettest scenarioWithInternalTransferlist")
       (#header .! "ManagedLedger nettest")
       (#parser .! clientParser)
   stablecoinContract <- prepareContract $ Just "test/resources/stablecoin.tz"
-  let safelistContractCompiled = convertContract $ compileLorentzContract safelistContract
+  externalTransferlistFile <- readFileUtf8 externalTransferlistContractPath
+  externalTransferlistContract <- either (error "cannot parse transferlist contract") pure $
+    parseExpandContract (Just externalTransferlistContractPath) externalTransferlistFile
   let
-    scenario :: NettestScenario
-    scenario impl = do
-      commentAction impl "Stablecoin contract nettest scenario"
-      scTransferScenario
-        (Unsafe.fromJust . mkInitialStorage) stablecoinContract safelistContractCompiled impl
+    originateTransferlistInternal transfers receivers =
+      originateUntypedSimple
+        "nettest.transferlist_internal"
+        (untypeValue . toVal $ Internal.Storage transfers receivers)
+        (convertContract $ compileLorentzContract Internal.transferlistContract)
+
+    originateTransferlistExternal :: forall m capsM. (Monad m, capsM ~ NettestT m) => Set (Address, Address) -> Set Address -> capsM Address
+    originateTransferlistExternal transfers receivers =
+      originateUntypedSimple
+        "nettest.transferlist_internal"
+        (untypeValue @(ToT External.Storage) . toVal
+          $ External.convertToExternalStorage
+              (Internal.Storage transfers receivers)
+              testOwner -- issuer
+              testOwner -- admin
+              )
+        externalTransferlistContract
+
+    scenarioWithInternalTransferlist :: NettestScenario
+    scenarioWithInternalTransferlist impl = do
+      commentAction impl "Stablecoin contract nettest scenarioWithInternalTransferlist"
+      scNettestScenario
+        (Unsafe.fromJust . mkInitialStorage) stablecoinContract originateTransferlistInternal impl
+
+    scenarioWithExternalTransferlist :: forall m capsM. (Monad m, capsM ~ NettestT m) => NettestImpl m -> m ()
+    scenarioWithExternalTransferlist impl = do
+      commentAction impl "Stablecoin contract nettest scenarioWithExternalTransferlist"
+      scNettestScenario
+        (Unsafe.fromJust . mkInitialStorage) stablecoinContract (originateTransferlistExternal @m @capsM) impl
 
   env <- mkMorleyClientEnv parsedConfig
-  runNettestViaIntegrational scenario
-  runNettestClient env scenario
+  runNettestViaIntegrational scenarioWithInternalTransferlist
+  runNettestViaIntegrational scenarioWithExternalTransferlist
+
+  runNettestClient env scenarioWithInternalTransferlist
+  runNettestClient env scenarioWithExternalTransferlist
